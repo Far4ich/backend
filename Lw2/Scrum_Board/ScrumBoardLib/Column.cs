7140dbeea4314d7f81f5fdfe4de9d6d39953ef4f
Far4ich <79937711+Far4ich@users.noreply.github.com>
2022-05-08 11:40:55 +0300

    Add Lw2

---
+++ Lw2/Scrum_Board/ScrumBoardLib/Column.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ScrumBoard
+{
+    internal class Column
+    {
+        private string name;
+        private readonly List<Task> _tasks;
+        public Column(string name)
+        {
+            this.name = name;
+            _tasks = new List<Task>();
+        }
+
+        public void Rename(string name)
+        {
+            this.name = name;
+        }
+        public string GetName()
+        {
+            return name;
+        }
+        public void AddTask(Task task)
+        {
+            _tasks.Add(task);
+        }
+        public Task RemoveTask(string title)
+        {
+            Task result;
+            bool taskIsFound = false;
+            foreach (Task task in _tasks)
+            {
+                if (task.Title == title)
+                {
+                    taskIsFound = true;
+                    result = task;
+                    _tasks.Remove(task);
+                    return result;
+                }
+            }
+            if (!taskIsFound)
+            {
+                throw new Exception("Task is not found");
+            }
+            return null;
+        }
+        public List<Task> GetTasks()
+        {
+            return _tasks;
+        }
+    }
+}
---
+++ Lw2/Scrum_Board/ScrumBoardLib/Task.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ScrumBoard
+{
+    internal class Task
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public Priority Priority { get; set; }
+
+        public Task(string title, string description, Priority priority)
+        {
+            this.Title = title;
+            this.Description = description;
+            this.Priority = priority;
+        }
+    }
+}
